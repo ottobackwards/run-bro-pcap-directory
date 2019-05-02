116a02a8901020cbd72011e6a4b423b979a38b1d
Otto Fowler <ottobackwards@gmail.com>
2019-05-02 11:04:50 -0400

    Update run_pcaps.sh

--- run_pcaps.sh
+++ run_pcaps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
- # Copyright 2019 Otto Fowler
+ # Copyright 2019 Otto Fowler and run_pcaps.sh contributors
  # All rights reserved.
  # Licensed under the Apache License, Version 2.0 (the "License");
  # you may not use this file except in compliance with the License.

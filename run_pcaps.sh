6a4746c79c48fdb8b1754a2750563c8dbb120cca
Otto Fowler <ottobackwards@gmail.com>
2019-05-02 11:04:16 -0400

    Update run_pcaps.sh

--- run_pcaps.sh
+++ run_pcaps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
- # Copyright 2018 simple-syslog authors
+ # Copyright 2019 Otto Fowler
  # All rights reserved.
  # Licensed under the Apache License, Version 2.0 (the "License");
  # you may not use this file except in compliance with the License.
